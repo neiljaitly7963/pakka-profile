f53bb8b594f607b8f843b51b0169952c7cf7f7bd
neiljaitly7963 <neiljaitly7963@gmail.com>
2020-08-19 22:23:02 +0530

    Updates

---
+++ precache-manifest.69e76c34d6740ddc8994a78b8744e6fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3862e18608c49f4c56713e108325633f",
+    "url": "/pakka-profile/index.html"
+  },
+  {
+    "revision": "e6d6be690308666c7cc6",
+    "url": "/pakka-profile/static/css/main.0abc507e.chunk.css"
+  },
+  {
+    "revision": "299624c98e4c51c0a7c0",
+    "url": "/pakka-profile/static/js/2.34516717.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pakka-profile/static/js/2.34516717.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6d6be690308666c7cc6",
+    "url": "/pakka-profile/static/js/main.64510c6f.chunk.js"
+  },
+  {
+    "revision": "5ae93b3e6c1614582d1e",
+    "url": "/pakka-profile/static/js/runtime-main.a5cdbbfa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pakka-profile/static/media/logo.5d5d9eef.svg"
+  }
+]);
